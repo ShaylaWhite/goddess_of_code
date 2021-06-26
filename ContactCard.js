2e554b9643cab983afbe5d18375152f03e18c17e
Shayla White <45671858+ShaylaWhite@users.noreply.github.com>
2021-06-25 18:49:35 -0700

    added props

--- ContactCard.js
+++ ContactCard.js
@@ -2,9 +2,18 @@ import React from "react"
 
 function ContactCard(props){
     return (
+        <div className="contact-card">
+            <img src={props.name}/>
+            <img src={props.date}/>
+            <img src={props.email/>
+        </div>
 
     )
 }
 
 
 export default ContactCard
+
+//props is a object that has propeties of that object 
+//contact card component 
+//
