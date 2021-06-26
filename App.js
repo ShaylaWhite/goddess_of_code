d8fda6b9eaacafd3cfab6e61ac3c98ca3f8a44ca
Shayla White <45671858+ShaylaWhite@users.noreply.github.com>
2021-06-25 18:45:53 -0700

    contact card and app.js

--- App.js
+++ App.js
@@ -1,1 +1,22 @@
-App.js
+import React from "react"
+import ContactCard from "./ContactCard"
+
+function App () {
+    return (
+
+        <div>
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+
+
+        </div>
+
+    )
+}
+
+export default App
+
+//import contact card component into the App.js file
+//component <contactcard />
---
+++ ContactCard.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function ContactCard(props){
+    return (
+
+    )
+}
+
+
+export default ContactCard
